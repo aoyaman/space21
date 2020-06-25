0cdfcad226608238c66069adcfaac40f7ea0545e
naoki aoyama <bluemountain@iMac.local>
2020-06-25 19:48:48 +0900

    storeの永続化対応

--- src/index.js
+++ src/index.js
@@ -8,19 +8,36 @@ import { createStore, applyMiddleware } from 'redux'
 import rootReducer from './reducers'
 import thunk from 'redux-thunk'
 import logger from 'redux-logger'
+import { persistReducer, persistStore } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+import { PersistGate } from 'redux-persist/integration/react'
 
 const middlewares = [thunk, logger];
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares))
+// 永続化の設定
+const persistConfig = {
+  key: 'root', // Storageに保存されるキー名を指定する
+  storage, // 保存先としてlocalStorageがここで設定される
+  // whitelist: ['todos'] // Stateは`todos`のみStorageに保存する
+  // blacklist: ['visibilityFilter'] // `visibilityFilter`は保存しない
+}
+
+// 永続化設定されたReducerとして定義
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+const store = createStore(
+  persistedReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  applyMiddleware(...middlewares))
+
+const persistor = persistStore(store)
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <AppContainer />
-      {/* <Router>
-        <Route path="/" component={GameIndexContainer} />
-        <Route path="/show/:id" component={GameShowComponent} />
-      </Router> */}
+      <PersistGate loading={null} persistor={persistor}>
+        <AppContainer />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
